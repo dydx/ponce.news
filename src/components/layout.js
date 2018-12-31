3aa383b6db2cd515bc9585b9de0479c86597121e
Josh Sandlin <joshua.sandlin@gmail.com>
2018-12-30 21:05:19 -0500

    gotta figure out how to add public to the damn .gitignore and not lose important stuff

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql, Link } from 'gatsby';
 
@@ -7,43 +7,95 @@ import Menu from './menu';
 import Footer from './theme/footer';
 import './styles/main.css';
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+class Layout extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      sidebarOpen: true
+    }
+  }
+
+  render () {
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
           }
-        }
-      }
-    `}
-    render={data => (
-        <div id="wrapper">
-          <div id="main">
-            <div className="inner">
-            { children }
+        `}
+        render={data => (
+            <div id="wrapper">
+              <div id="main">
+                <div className="inner">
+                { this.props.children }
+                </div>
+              </div>
+    
+              <div id="sidebar" className={(this.state.sidebarOpen) ? '' : 'inactive'}>
+
+                <a className="toggle" onClick={() => this.setState({ sidebarOpen: !this.state.sidebarOpen })}>Toggle</a>
+                <div className="inner">
+                  <section id="search" className="alt">
+                    <Link to="/live-broadcast" className="button fit">Live Broadcast</Link>
+                  </section>
+    
+    
+                  <Menu />
+    
+                  <Featured />
+    
+                  <Footer siteTitle={data.site.siteMetadata.title} />
+                </div>
+              </div>
             </div>
-          </div>
+        )}
+      />
+    )
+  }
+}
 
-          <div id="sidebar">
-            <div className="inner">
-              <section id="search" className="alt">
-                <Link to="/live-broadcast" className="button fit">Live Broadcast</Link>
-              </section>
+// const Layout = ({ children }) => (
+//   <StaticQuery
+//     query={graphql`
+//       query SiteTitleQuery {
+//         site {
+//           siteMetadata {
+//             title
+//           }
+//         }
+//       }
+//     `}
+//     render={data => (
+//         <div id="wrapper">
+//           <div id="main">
+//             <div className="inner">
+//             { children }
+//             </div>
+//           </div>
+
+//           <div id="sidebar" className="">
+//           <a className="toggle">Toggle</a>
+//             <div className="inner">
+//               <section id="search" className="alt">
+//                 <Link to="/live-broadcast" className="button fit">Live Broadcast</Link>
+//               </section>
 
 
-              <Menu />
+//               <Menu />
 
-              <Featured />
+//               <Featured />
 
-              <Footer siteTitle={data.site.siteMetadata.title} />
-            </div>
-          </div>
-        </div>
-    )}
-  />
-)
+//               <Footer siteTitle={data.site.siteMetadata.title} />
+//             </div>
+//           </div>
+//         </div>
+//     )}
+//   />
+// )
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
